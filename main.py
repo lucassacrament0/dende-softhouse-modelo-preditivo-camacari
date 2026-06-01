0097f4c4b7bf1d177e80038e3e9e88ed8eff9a77
Elias Barreto <eliasguideonbarreto@gmail.com>
2026-05-31 23:10:00 -0300

    Retirado recurso que entrou em desuso.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Carregando os dados
 train_df = pd.read_csv('train.csv')
